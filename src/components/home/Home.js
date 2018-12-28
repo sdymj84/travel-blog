4c36fe286bf869a2e5c98f622ba2d80d88b0bd95
Minjun Youn <minjun1.y@partner.samsung.com>
2018-12-28 17:03:40 -0600

    map click and scroll to country card

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
-import { Link } from "react-router-dom";
 import styled from 'styled-components'
-import { Container, Image, Card, Row, Col } from 'react-bootstrap'
+import { Container, Row } from 'react-bootstrap'
 import SvgComponent from './SvgComponent'
+import CountryCards from './CountryCards'
 
 const StyledContainer = styled.div`
   .container {
@@ -106,82 +106,30 @@ const StyledWorldMap = styled(SvgComponent)`
   
 `
 
-const StyledLink = styled(Link)`
-  .card {
-    display: inline-block;
-    vertical-align: middle;
-    -webkit-transform: perspective(1px) translateZ(0);
-    transform: perspective(1px) translateZ(0);
-    box-shadow: 0 0 1px rgba(0, 0, 0, 0);
-    -webkit-transition-duration: 0.3s;
-    transition-duration: 0.3s;
-    -webkit-transition-property: box-shadow, transform;
-    transition-property: box-shadow, transform;
-  }
-
-  .card:hover, .card:focus, .card:active {
-    text-decoration: none;
-    -webkit-transform: scale(1.02);
-    transform: scale(1.02);
-    box-shadow: 1px 1px 20px grey;
-  }
-
-`
-
 export class Home extends Component {
+
+  ref = React.createRef();
+
+  scrollToRef = () => {
+    window.scrollTo({
+      top: this.ref.current.offsetTop,
+      behavior: "smooth"
+    })
+  }
+
+  handleClick = () => {
+    this.scrollToRef()
+  }
+
   render() {
     return (
       <StyledContainer>
         <div className='jumbo'>
-          {/* <StyledWorldMap src="img/worldmap.jpg" useMap="#image-map" fluid /> */}
-          <StyledWorldMap />
+          <StyledWorldMap handleClick={this.handleClick} />
         </div>
         <Container>
-          <Row>
-
-            <Col sm={6} lg={4}>
-              <StyledLink to="/post/south-korea">
-                <Card>
-                  <Card.Img variant="top" src="img/south-korea.jpg" />
-                  <Card.Body>
-                    <Card.Title>South Korea</Card.Title>
-                    <Card.Text>
-                      My Home
-                </Card.Text>
-                  </Card.Body>
-                </Card>
-              </StyledLink>
-            </Col>
-
-            <Col sm={6} lg={4}>
-              <StyledLink to="post/autralia">
-                <Card>
-                  <Card.Img variant="top" src="img/australia.jpg" />
-                  <Card.Body>
-                    <Card.Title>Australia</Card.Title>
-                    <Card.Text>
-                      Been there for 1y 6m
-                </Card.Text>
-                  </Card.Body>
-                </Card>
-              </StyledLink>
-            </Col>
-
-            <Col sm={6} lg={4}>
-              <StyledLink to="/post/usa">
-                <Card>
-                  <Card.Img variant="top" src="img/usa.jpg" />
-                  <Card.Body>
-                    <Card.Title>United States</Card.Title>
-                    <Card.Text>
-                      My second home
-                </Card.Text>
-                  </Card.Body>
-                </Card>
-              </StyledLink>
-            </Col>
-
-          </Row>
+          <div ref={this.ref}></div>
+          <CountryCards />
         </Container>
       </StyledContainer >
     )
