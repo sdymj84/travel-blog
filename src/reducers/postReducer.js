4a82d452cae1c7f9ff973c98a48f12cf9a071b1a
sdymj84 <sdymj84@gmail.com>
2018-12-29 09:33:11 -0600

    adding reducers

---
+++ src/reducers/postReducer.js
@@ -0,0 +1,28 @@
+const initialState = {
+  countries: [
+    {
+      id: 1,
+      country: "South Korea",
+      summary: "My Home",
+      continent: "asia",
+    }
+  ],
+
+  posts: [
+    {
+      id: 100,
+      title: "Seoul 3 days trip",
+      summary: "Eat Eat and Eat!!!",
+      content: [
+        {
+          image: "",
+          body: "1",
+        },
+        {
+          image: "",
+          body: "2",
+        }
+      ]
+    }
+  ]
+}
