897159400be0676ea2b0a29a8a1ca43df765a037
Minjun Youn <minjun1.y@partner.samsung.com>
2019-02-06 11:01:35 -0600

    complete add post to stroage and db

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -7,43 +7,58 @@ export const createPost = (post, history) => {
 
     // slugify country name and add to db
     const countrySlug = slugify(post.country, { lower: true })
-    const ref = firebase.storage().ref().child(`posts/${post.selectedFile.name}`)
 
     const uploadMain = new Promise((resolve, reject) => {
+      // set ref for main image
+      const ref = firebase.storage().ref()
+        .child(`posts/${post.selectedFile.name}`)
       // add image to firebase storage
       ref.put(post.selectedFile).then(() => {
         delete post.selectedFile
         ref.getDownloadURL().then(url => {
+          // change file prop to url because file object cannot be uploaded to db
           post.mainImage = url
           resolve("main image upload finish")
         })
+      }).catch(err => {
+        const msg = "Error while adding main image to storage :" + err
+        reject(msg)
       })
     })
 
     const uploadContent = new Promise((resolve, reject) => {
       const contentsLen = post.contents.length
       post.contents.map((content, i) => {
+        const ref = firebase.storage().ref()
+          .child(`posts/${content.image.name}`)
         ref.put(content.image).then(() => {
           ref.getDownloadURL().then(url => {
             content.image = url
+            console.log(content.image)
+            console.log('i :', i)
             if (contentsLen === i + 1) {
+              console.log('now resolve, i: ', i)
               resolve("content images upload finish")
             }
           })
+        }).catch(err => {
+          const msg = "Error while adding content images to storage :" + err
+          reject(msg)
         })
       })
     })
 
+    // execute db insert logic after storage logic is finished
     Promise.all([uploadMain, uploadContent]).then((msg) => {
       console.log(msg)
-      post.contents = post.contents.map(content => {
-        return Object.assign({}, content)
-      })
+      console.log('posts :', post)
+      console.log('db :', db)
       db.collection('posts').add({
         ...post,
         countrySlug: countrySlug,
         createdAt: new Date()
       }).then((doc) => {
+        console.log("got in db then")
         // set 'howManyPosts' in country doc in countries db
         const countryRef = db.collection('countries').where('countryName', '==', post.country).limit(1)
         countryRef.get().then(querySnapshot => {
@@ -53,6 +68,7 @@ export const createPost = (post, history) => {
             })
           })
         })
+        console.log("db added")
         post.id = doc.id
         // redirect to post detail page when db work is done
         history.push(`/post/${post.country}/${doc.id}`)
@@ -61,15 +77,6 @@ export const createPost = (post, history) => {
         dispatch({ type: 'ADD_POST_ERROR', err })
       })
     })
-
-
-    // after adding image, add post to db including image url from storage
-    // ref.getDownloadURL().then(url => {
-
-
-
-    // })
-    // })
   }
 }
 
